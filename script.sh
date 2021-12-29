b52258c0f2e07468e44a7270218540a82d0bf5e7
Josh <66495148+Joshwgaming@users.noreply.github.com>
2021-12-29 14:33:03 +0000

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get upgrade -y
+
+sudo apt install curl -y
+
+sudo curl -sL https://deb.nodesource.com/setup_16.x| sudo -E bash -
+
+sudo apt install nodejs -y
+
+reboot
